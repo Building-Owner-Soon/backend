93d2501f199a27f95ce0ce7f80391abe01320d9b
khcdev <chakh9601@gmail.com>
2025-07-13 19:32:37 +0900

    feat: sign-up, sign-in API representation

--- src/main/kotlin/com/bos/backend/presentation/auth/controller/AuthController.kt
+++ src/main/kotlin/com/bos/backend/presentation/auth/controller/AuthController.kt
@@ -1,32 +1,29 @@
 package com.bos.backend.presentation.auth.controller
 
+import com.bos.backend.application.auth.AuthService
 import com.bos.backend.presentation.auth.dto.CommonSignResponseDTO
-import com.bos.backend.presentation.auth.dto.MeResponse
 import com.bos.backend.presentation.auth.dto.SignInRequestDTO
 import com.bos.backend.presentation.auth.dto.SignUpRequestDTO
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class AuthController {
+class AuthController(
+    private val authService: AuthService,
+) {
     @PostMapping("/auth/sign-up")
     @ResponseStatus(HttpStatus.CREATED)
-    suspend fun signUp(signUpRequestDTO: SignUpRequestDTO): CommonSignResponseDTO {
-        TODO()
-    }
+    suspend fun signUp(signUpRequestDTO: SignUpRequestDTO): CommonSignResponseDTO = authService.signUp(signUpRequestDTO)
 
     @PostMapping("/auth/sign-in")
     @ResponseStatus(HttpStatus.OK)
-    suspend fun signIn(signInRequestDTO: SignInRequestDTO): CommonSignResponseDTO {
-        TODO()
-    }
-
-    @GetMapping("/me")
-    @ResponseStatus(HttpStatus.OK)
-    suspend fun getMe(): MeResponse {
-        TODO()
-    }
+    suspend fun signIn(signInRequestDTO: SignInRequestDTO): CommonSignResponseDTO = authService.signIn(signInRequestDTO)
+//
+//    @GetMapping("/me")
+//    @ResponseStatus(HttpStatus.OK)
+//    suspend fun getMe(): MeResponse {
+//        TODO()
+//    }
 }
