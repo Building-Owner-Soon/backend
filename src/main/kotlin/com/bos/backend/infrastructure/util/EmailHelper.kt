0cb39ee3d6f99b599768f2b9728d840514192eb4
Seoyul Kim <zoeyul07@gmail.com>
2025-07-25 16:28:50 +0900

    feat: email helper 함수와 template 추가

---
+++ src/main/kotlin/com/bos/backend/infrastructure/util/EmailHelper.kt
@@ -0,0 +1,28 @@
+package com.bos.backend.infrastructure.util
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Component
+
+@Component
+class EmailHelper(
+    private val mailSender: JavaMailSender,
+    @Value("\${spring.mail.username}") private val fromEmail: String,
+) {
+    
+    suspend fun sendEmail(
+        to: String,
+        subject: String,
+        content: String
+    ) {
+        val message = SimpleMailMessage().apply {
+            setFrom(fromEmail)
+            setTo(to)
+            this.subject = subject
+            text = content
+        }
+        
+        mailSender.send(message)
+    }
+} 
