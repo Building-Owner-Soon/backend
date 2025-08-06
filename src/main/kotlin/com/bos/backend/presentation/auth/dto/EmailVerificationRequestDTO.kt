dd1b6adf666692de1e2de1979884515affb7dcf8
Seoyul Kim <zoeyul07@gmail.com>
2025-08-06 22:31:52 +0900

    fix: type enum으로 NotBlack 가 아닌 NotNull 사용하도록 변경

--- src/main/kotlin/com/bos/backend/presentation/auth/dto/EmailVerificationRequestDTO.kt
+++ src/main/kotlin/com/bos/backend/presentation/auth/dto/EmailVerificationRequestDTO.kt
@@ -3,12 +3,13 @@ package com.bos.backend.presentation.auth.dto
 import com.bos.backend.domain.auth.enum.EmailVerificationType
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class EmailVerificationRequestDTO(
     @field:Email
     @field:NotBlank
     val email: String,
-    @field:NotBlank
+    @field:NotNull
     val type: EmailVerificationType,
 )
 
@@ -18,6 +19,6 @@ data class EmailVerificationCheckDTO(
     val email: String,
     @field:NotBlank
     val code: String,
-    @field:NotBlank
+    @field:NotNull
     val type: EmailVerificationType,
 )
