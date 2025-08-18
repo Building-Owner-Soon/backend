514fdda772cc9c0d278c20a627bc4bf03dcc673a
Seoyul Kim <zoeyul07@gmail.com>
2025-08-18 21:59:32 +0900

    refactor: delete시 coroutineRepository 의 save 메소드를 통해 업데이트 하도록 변경

--- src/main/kotlin/com/bos/backend/infrastructure/persistence/R2dbcUserRepository.kt
+++ src/main/kotlin/com/bos/backend/infrastructure/persistence/R2dbcUserRepository.kt
@@ -2,22 +2,10 @@ package com.bos.backend.infrastructure.persistence
 
 import com.bos.backend.domain.user.entity.User
 import com.bos.backend.domain.user.repository.UserRepository
-import org.springframework.data.r2dbc.repository.Modifying
-import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.time.Instant
 
-interface UserCoroutineRepository : CoroutineCrudRepository<User, Long> {
-    @Modifying
-    @Query("UPDATE users SET deleted_at = :deletedAt, updated_at = :updatedAt WHERE id = :userId")
-    suspend fun deleteUser(
-        @Param("userId") userId: Long,
-        @Param("deletedAt") deletedAt: Instant,
-        @Param("updatedAt") updatedAt: Instant,
-    ): Int
-}
+interface UserCoroutineRepository : CoroutineCrudRepository<User, Long>
 
 @Repository
 class R2dbcUserRepositoryImpl(
@@ -30,6 +18,6 @@ class R2dbcUserRepositoryImpl(
     override suspend fun deleteById(id: Long) {
         val user = findById(id) ?: return
         val deletedUser = user.delete()
-        coroutineRepository.deleteUser(id, deletedUser.deletedAt!!, deletedUser.updatedAt)
+        coroutineRepository.save(deletedUser)
     }
 }
