301f076dd6192bbbc82a9638ea24d0543436b6b6
khcdev <chakh9601@gmail.com>
2025-08-24 15:30:00 +0900

    chore: cors 적용

--- src/main/kotlin/com/bos/backend/infrastructure/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/bos/backend/infrastructure/config/SecurityConfiguration.kt
@@ -1,11 +1,16 @@
 package com.bos.backend.infrastructure.config
 
 import com.bos.backend.infrastructure.security.JwtSecurityContextRepository
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebFluxSecurity
@@ -13,12 +18,16 @@ class SecurityConfiguration(
     private val jwtSecurityContextRepository: JwtSecurityContextRepository,
 ) {
     @Bean
-    fun securityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+    fun securityFilterChain(
+        http: ServerHttpSecurity,
+        corsConfig: CorsConfigurationSource,
+    ): SecurityWebFilterChain =
         http
             .csrf { it.disable() }
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
             .logout { it.disable() }
+            .cors { it.configurationSource(corsConfig) }
             .authorizeExchange {
                 it
                     .pathMatchers(
@@ -35,4 +44,27 @@ class SecurityConfiguration(
                     .authenticated()
             }.securityContextRepository(jwtSecurityContextRepository)
             .build()
+
+    @Bean
+    fun corsConfig(corsProperties: CorsProperties): CorsConfigurationSource {
+        val config =
+            CorsConfiguration().apply {
+                allowedOrigins = corsProperties.allowedOrigins
+                allowedMethods = corsProperties.allowedMethods
+                allowedHeaders = corsProperties.allowedHeaders
+                allowedOriginPatterns = corsProperties.allowedOriginPatterns
+                allowCredentials = true
+            }
+
+        return UrlBasedCorsConfigurationSource().apply { registerCorsConfiguration("/**", config) }
+    }
 }
+
+@Component
+@ConfigurationProperties(prefix = "cors")
+data class CorsProperties(
+    var allowedOrigins: List<String> = listOf(),
+    var allowedMethods: List<String> = listOf(),
+    var allowedHeaders: List<String> = listOf(),
+    var allowedOriginPatterns: List<String> = listOf(),
+)
