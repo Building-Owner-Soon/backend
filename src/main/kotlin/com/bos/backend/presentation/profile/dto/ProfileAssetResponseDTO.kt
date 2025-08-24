166535fb9a6b0910a184c1a2482716a478d8efb3
Seoyul Kim <zoeyul07@gmail.com>
2025-08-24 14:56:48 +0900

    refactor: profileAssetType 변경에 따른 respnse dto 변경

--- src/main/kotlin/com/bos/backend/presentation/profile/dto/ProfileAssetResponseDTO.kt
+++ src/main/kotlin/com/bos/backend/presentation/profile/dto/ProfileAssetResponseDTO.kt
@@ -2,9 +2,9 @@ package com.bos.backend.presentation.profile.dto
 
 data class ProfileAssetResponseDTO(
     val home: List<AssetDTO>,
-    val faceShape: List<AssetDTO>,
+    val face: List<AssetDTO>,
     val hand: List<AssetDTO>,
-    val frontHair: List<AssetDTO>,
+    val bang: List<AssetDTO>,
     val backHair: List<AssetDTO>,
     val eyes: List<AssetDTO>,
     val mouth: List<AssetDTO>,
